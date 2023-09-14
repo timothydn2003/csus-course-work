8362e1fd22aa5507eb0a6940aebbec8a7a7cfa7c
Timothy Doan <87452000+timothydn2003@users.noreply.github.com>
2023-09-14 12:25:41 -0700

    Add files via upload

---
+++ CSC 15/Tennessee/TennesseeDoan.java
@@ -0,0 +1,61 @@
+/* 
+Timothy Doan
+September 12, 2021
+This program prints the word Tennessee letter by letter.
+100/100 because I followed all the guidelines and included all the requirments from both rubric and assignment description.
+*/
+public class TennesseeDoan{
+    //This is the main method and it will run all of the other static methods printing out the word Tennessee
+    public static void main(String[] args){
+        printT();
+        printE();
+        printN();
+        printN();
+        printE();
+        printS();
+        printS();
+        printE();
+        printE();
+    }
+    //This method going to print the letter T
+    public static void printT(){
+        System.out.println("TTTTTTTTTT");
+        System.out.println("    TT");
+        System.out.println("    TT");
+        System.out.println("    TT");
+        System.out.println("    TT");
+        System.out.println("    TT");
+        System.out.println();
+    }
+    //This method is going to print the letter E
+    public static void printE(){
+        System.out.println("EEEEEEEEEE");
+        System.out.println("E");
+        System.out.println("EEEEEEEEEE");
+        System.out.println("E");
+        System.out.println("EEEEEEEEEE");
+        System.out.println();
+    }
+    //This method is going to print letter N
+    public static void printN(){
+        System.out.println("N N    N");
+        System.out.println("N  N   N");
+        System.out.println("N   N  N");
+        System.out.println("N    N N");
+        System.out.println("N      N");
+        System.out.println();
+    }
+    //This method is going to print letter S
+    public static void printS(){
+        System.out.println(" SSSSSSSS");
+        System.out.println("S        S");
+        System.out.println("S");
+        System.out.println("S");
+        System.out.println(" SSSSSSSSS");
+        System.out.println("         S");
+        System.out.println("         S");
+        System.out.println("S        S");
+        System.out.println("SSSSSSSSSS");
+        System.out.println();
+    }
+}
