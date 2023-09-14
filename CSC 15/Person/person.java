1a5510e76ee077fe7c361529ff6c4c0d27f2117c
Timothy Doan <87452000+timothydn2003@users.noreply.github.com>
2023-09-14 12:21:11 -0700

    Add files via upload

---
+++ CSC 15/Person/person.java
@@ -0,0 +1,44 @@
+/*
+    Timothy Doan
+    CSC 15, Fall 2021
+    Professor Faroughi
+    This program prints the picture of the person 
+    100/100
+*/
+public class person { // this line declares the class
+    public static void main(String[] args){ // this line declares the main method
+        //this section will print the top of the persons head
+        System.out.println("         /\"\\");
+        System.out.println("        /\"_\"\\");
+        System.out.println("       /\"_\"_\"\\");
+        System.out.println("      /\"_\"_\"_\"\\");
+        System.out.println("     /\"_\"_\"_\"_\"\\");
+        System.out.println("    /\"_\"_\"_\"_\"_\"\\");
+        //this section will print the actual head of the person
+        System.out.println("      |||||||||");
+        System.out.println("     | ~      ~ |");
+        System.out.println("    @| O  /\\  O |@");
+        System.out.println("     |   ----   |");
+        System.out.println("     |__________|");
+        //this section will print the neck and body of the person
+        System.out.println("         | |        ");
+        System.out.println("     ___ | | ___    ");
+        System.out.println("    |           |   ");
+        System.out.println("   /|   Peace   |\\ ");
+        System.out.println("  / |___________| \\");
+        //this section will print the hands and legs of the person
+        System.out.println(" *     ||   ||     *");
+        System.out.println("       ||   ||      ");
+        System.out.println("       ||   ||      ");
+        System.out.println("       /\\   /\\    ");
+        System.out.println("       /\\   /\\    ");
+        System.out.println("       \\/   \\/    ");
+        //this section will print out the bottom triangle of the person
+        System.out.println("    \\\"_\"_\"_\"_\"_\"/");
+        System.out.println("     \\\"_\"_\"_\"_\"/");
+        System.out.println("      \\\"_\"_\"_\"/");
+        System.out.println("       \\\"_\"_\"/");
+        System.out.println("        \\\"_\"/");
+        System.out.println("         \\\"/");
+    }
+}
